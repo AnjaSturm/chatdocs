b937f514fe915ed4d2d87a933117eedc1df5cae3
Anja Sturm <anja@familie-sturm.de>
2023-08-22 23:52:39 +0200

    updated startup script

--- docker/chatdocs/startup.bash
+++ docker/chatdocs/startup.bash
@@ -5,4 +5,9 @@ if [[ ! -f control/delete-this-to-download-again ]]; then
   touch control/delete-this-to-download-again
 fi
 
+if [[ ! -f control/delete-this-to-add-documents-again ]]; then
+  python -m chatdocs add documents/
+  touch control/delete-this-to-add-documents-again
+fi
+
 python -m chatdocs ui
