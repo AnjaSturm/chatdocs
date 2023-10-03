30e2d421aa4ab75f5a7ac168327b23841b79ddc8
Anja Sturm <anja@familie-sturm.de>
2023-10-03 18:03:21 +0200

    Zwischenstand: get rid of websocket

--- chatdocs/ui.py
+++ chatdocs/ui.py
@@ -4,21 +4,11 @@ from queue import Queue
 from threading import Thread
 from typing import Any, Dict
 
-from quart import Quart, render_template, websocket
+from quart import Quart, render_template, request
 from rich import print
 from rich.panel import Panel
 
 from .chains import get_retrieval_qa
-
-
-async def receive():
-    data = await websocket.receive()
-    return json.loads(data)
-
-
-async def send(data: Any):
-    data = json.dumps(data)
-    await websocket.send(data)
 
 
 def ui(config: Dict[str, Any]) -> None:
@@ -39,35 +29,38 @@ def ui(config: Dict[str, Any]) -> None:
     @app.get("/")
     async def index():
         return await render_template("index.html")
+    
+    # @app.route("/login")
+    # def login():
+    #     return "login"
+        
+    @app.route("/query", methods=["POST"])
+    async def query():
+        req = await request.get_json()
+        id, query = req["id"], req["query"]
+        # if auth and auth != req.get("auth"):
+        #     print("Authentication error.")
+        #     return
+        Thread(target=worker, daemon=True, args=(query,)).start()
 
-    @app.websocket("/ws")
-    async def ws() -> None:
-        while True:
-            req = await receive()
-            id, query = req["id"], req["query"]
-            if auth and auth != req.get("auth"):
-                print("Authentication error.")
-                return
-            Thread(target=worker, daemon=True, args=(query,)).start()
+        done = False
+        while not done:
+            data = q.get()
+            res = {"id": id}
+            if isinstance(data, str):
+                res["chunk"] = data
+            else:
+                res["result"] = data["result"]
+                res["sources"] = sources = []
+                for doc in data["source_documents"]:
+                    source, content = doc.metadata["source"], doc.page_content
+                    sources.append({"source": source, "content": content})
+                done = True
+            q.task_done()
 
-            done = False
-            while not done:
-                data = q.get()
-                res = {"id": id}
-                if isinstance(data, str):
-                    res["chunk"] = data
-                else:
-                    res["result"] = data["result"]
-                    res["sources"] = sources = []
-                    for doc in data["source_documents"]:
-                        source, content = doc.metadata["source"], doc.page_content
-                        sources.append({"source": source, "content": content})
-                    done = True
-
-                await send(res)
-                q.task_done()
+            return json.dumps(res)
 
     host, port = config["host"], config["port"]
-    if auth:
-        print(Panel(f"Visit [bright_blue]http://localhost:{port}?auth={auth}"))
-    app.run(host=host, port=port, use_reloader=False)
+    # if auth:
+    #     print(Panel(f"Visit [bright_blue]http://localhost:{port}?auth={auth}"))
+    app.run(host=host, port=port, use_reloader=False, debug=True)
