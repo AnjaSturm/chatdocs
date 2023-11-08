22c4d0fae51c94f95db20726f7900a9cb59e8d7a
Anja Sturm <anja@familie-sturm.de>
2023-11-08 17:39:41 +0100

    tidy up chains

--- chatdocs/chains.py
+++ chatdocs/chains.py
@@ -5,13 +5,10 @@ from langchain.prompts import PromptTemplate
 from langchain.memory import ConversationBufferMemory
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler  # for streaming response
 from langchain.callbacks.manager import CallbackManager
-from chromadb.config import Settings
 
 
 from .llms import get_llm
 from .vectorstores import get_collection
-import time
-import chromadb
 
 
 callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
@@ -65,81 +62,16 @@ def get_retrieval_qa(
     callback: Optional[Callable[[str], None]] = None,
 ) -> RetrievalQA:
 #[RetrievalQA, RetrievalQA]:
-    # tic = time.perf_counter()
     db = get_collection(config, collection_name)
-    collection = db.get()
-    print(db._collection.name, "COLLECTION_NAME")
-    print(db._collection.count(), "COLLECTION_NAME")
-    
-    print(db._client, "CLIENTCOUNT")
-    print(db.embeddings, "EMBEDDINGS")
-    
-    
-    # print(collection.count(), "COLLECTION_NAME") greht nicht    
-    # print(collection["metadatas"][0], "COLLECTIONFULL")
-    
-    
-    # list = []
-    # # list.append(metadata["source"] for metadata in collection["metadatas"])
-    # source = collection["metadatas"][0]["source"]
-    # document = collection["documents"][0]
-    # document1 = collection["documents"][1]
-    
-    # # data = db._collection.get(include=['documents'])
-    # print(source, "SOURCE")
-    # print(document, "DOCUMENT")
-    # print(document1, "DOCUMENT1")
-    
-    
-    
-    # collection = db.get()
-    # client = chromadb.Client(Settings(persist_directory="db"))
-    # col = db._collection.name
-    # print(db._collection, "COLLECTION")
-    # print(collection['metadata'], "COLLECTION")
-    
-        
-    
-    # db1 = get_vectorstore(config)
-    
-    # toc = time.perf_counter()
-    # print(f"Get vectorstore in {toc - tic:0.4f} seconds")
-    # tic2 = time.perf_counter()
     retriever = db.as_retriever(**config["retriever"]) # config übergibt anzahl an documenten -> 4
-    
-    # retriever1 = db1.as_retriever(**config["retriever"])
-    
-    # toc2 = time.perf_counter()
-    # print(f"Get retriever in {toc2 - tic2:0.4f} seconds")
-    # tic3 = time.perf_counter()
     llm = get_llm(config, callback=callback)
-    #llm = get_llm(config)
-    
-    # toc3 = time.perf_counter()
-    # print(f"Get llm in {toc3 - tic3:0.4f} seconds")
-    # print(f"Getting db, retriever, llm in summary: {toc3 - tic:0.4f} seconds")
-    
-   
-    # return [RetrievalQA.from_chain_type(
-    #     llm=llm,
-    #     retriever=retriever,
-    #     return_source_documents=True,
-    #     chain_type_kwargs={
-    #         "verbose": True,
-    #         "prompt": prompt,
-    #         "memory": ConversationBufferMemory(
-    #             memory_key="history",
-    #             input_key="question"
-    #         )
-    #     }
-    # ), chain1]
 
     qa = RetrievalQA.from_chain_type(
         llm=llm,
         retriever=retriever,
         return_source_documents=True,
-        verbose = True, #what does this do?
-        callbacks=callback_manager, # same here
+        verbose = True,
+        callbacks=callback_manager,
         chain_type_kwargs={
             "verbose": True,
             "prompt": prompt,
