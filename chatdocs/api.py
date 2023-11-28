00115d3838ebb978224a42c2f049d3c1441cecc4
Anja Sturm <anja@familie-sturm.de>
2023-11-28 09:38:16 +0100

    vectorstore handling

--- chatdocs/api.py
+++ chatdocs/api.py
@@ -19,10 +19,9 @@ from quart_jwt_extended import (
 from rich import print
 
 from .chains import get_retrieval_qa
-from .add import add, delete
+from .add import add, delete, delete_file
 from .llms import get_llm
 from .vectorstores import get_collection
-from .embeddings import get_embeddings
 
 
 DOCUMENT_DIRECTORY = Path(__file__).resolve().parent.parent / "documents"
@@ -32,9 +31,8 @@ ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 def api(config: Dict[str, Any]) -> None:
     
-    # instantiate llm and embeddings
+    # instantiate llm and qa
     llm = get_llm(config)
-    embeddings = get_embeddings(config)
     session_context = {}
     
         
@@ -63,10 +61,10 @@ UijGy2974VspHY+XrggzbKT2wzo8GNiFx16vuZdPNyXrZxUkqKjNZxpXvpJQ5YW6
     q = Queue()
     
     # Block until all tasks are done.
-    def putWorkInQueueAndWaitForDone(session_id: int, query: str):
+    def putWorkInQueueAndWaitForDone(query: str):
         event = Event()
-        qa = session_context[session_id]["qa"]
-        task = {'id': session_id, 'query': query}
+        qa = session_context["qa"]
+        task = {'id': str(uuid.uuid4()), 'query': query}
         q.put(partial(work, event, qa, task))
         event.wait()
         return task
@@ -88,9 +86,6 @@ UijGy2974VspHY+XrggzbKT2wzo8GNiFx16vuZdPNyXrZxUkqKjNZxpXvpJQ5YW6
     def allowed_file(filename):
         return '.' in filename and \
             filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-            
-    def get_directory(directory):
-        return DOCUMENT_DIRECTORY / directory
 
     def getUserData(token):
         url = "https://api.mdoo.dev.s8l.tech/user/profile"
@@ -105,49 +100,52 @@ UijGy2974VspHY+XrggzbKT2wzo8GNiFx16vuZdPNyXrZxUkqKjNZxpXvpJQ5YW6
         company = labels["customer"]
         return company, admin
 
-    def initUser(jwt_identity: any, headers: any):
-        print("jwt_identity: " + str(jwt_identity))
-        session_id = session.get('session_id', str(jwt_identity))
-        # session_id = session.get('session_id', str(        uuid.uuid4()))
-        session['session_id'] = session_id
+    def initUser(headers: any):
+        company, admin = getUserData(headers["Authorization"].split(" ")[1])
+        if 'session_id' not in session:
+            session['session_id'] = str(        uuid.uuid4())
+        if 'company' not in session:
+            session['company'] = company
+        if 'admin' not in session:
+            session['admin'] = admin
+        
+        session_id = session['session_id']
         print("Query for session id " + session_id)
         
         if (session_id not in session_context):
             print("Generating new session data")
-            company, admin = getUserData(headers["Authorization"].split(" ")[1])
-            session_context[session_id] = { "qa": get_retrieval_qa(config, llm, get_collection(config, company, embeddings)), "company": company, "admin": admin}
+            session_context['session_id'] = session_id
+            session_context["qa"] = get_retrieval_qa(config, llm, get_collection(company))
             
         else:
             print("Using existing session data")
         print("Session data for current request: " + str(session_context)) 
         
         
-        
     @app.route("/login", methods=["POST"])
     @jwt_required
     async def login():
         jwt_identity = get_jwt_identity()
-        initUser(jwt_identity, request.headers)
-        return "logged in successfully", 200
-    
-    
+        print("jwt_identity: " + str(jwt_identity))
+        initUser(request.headers)
+        return jsonify({"message": "logged in successfully"}), 200
+
+
     @app.route("/logout", methods=["POST"])
     @jwt_required
     async def logout():
-        jwt_identity = get_jwt_identity()
-        session.pop('session_id', None)
-        session_context.pop(jwt_identity)
-        return "logged out successfully", 200
+        session.clear()
+        session_context.clear()
+        return jsonify({"message": "logged out successfully"}), 200
     
     
     @app.route("/query", methods=["POST", "GET"])
     @jwt_required
     async def query():
-        jwt_identity = get_jwt_identity()
         req = await request.form
         query = req["query"]
-        data = putWorkInQueueAndWaitForDone(str(jwt_identity), query)
-        res = {"id": str(jwt_identity)}
+        data = putWorkInQueueAndWaitForDone(query)
+        res = {"id": data["id"]}
         res["result"] = data["result"]["result"]
         res["sources"] = sources = []
         for doc in data["result"]["source_documents"]:
@@ -162,9 +160,10 @@ UijGy2974VspHY+XrggzbKT2wzo8GNiFx16vuZdPNyXrZxUkqKjNZxpXvpJQ5YW6
     @app.route("/files", methods=["GET", "POST", "DELETE"])
     @jwt_required
     async def manage_files():
-        jwt_identity = get_jwt_identity()
-        # company_name = session_context[jwt_identity]["company"]   
-        company_directory = DOCUMENT_DIRECTORY / "straightdocs-testcutomer"    
+        if not session["admin"]:
+            return jsonify({"message": "Unauthorized, only allowed for admin"}), 401
+        company_name = session["company"]   
+        company_directory = DOCUMENT_DIRECTORY / company_name  
         
         if request.method == "POST":
             if not company_directory.exists():
@@ -172,21 +171,25 @@ UijGy2974VspHY+XrggzbKT2wzo8GNiFx16vuZdPNyXrZxUkqKjNZxpXvpJQ5YW6
                 
             req = await request.files
             if 'file' not in req:
-                return 'No file', 400
+                return jsonify({"message": 'No file'}), 400
+
             file = req['file']
             if file.filename == '':
-                return 'No file selected', 400
+                return jsonify({"message": 'No file selected'}), 400
+            
             filename = secure_filename(file.filename)
             file_directory = company_directory / filename
             if file_directory.is_file():
-                return 'File already exists', 409
+                return jsonify({"message": 'File already exists'}), 409
+            
             if not allowed_file(filename):
-                return 'File type not allowed', 415
+                return jsonify({"message": 'File type not allowed'}), 415
+            
             try:
                 await file.save(file_directory)
-                return "File uploaded successfully", 201
+                return jsonify({"message": f"File {str(filename)} uploaded successfully"}), 201
             except Exception as e:
-                return f"Failed to upload file: {str(e)}", 500
+                return jsonify({"message": f"Failed to upload file: {str(e)}"}), 500
 
         elif request.method == "GET":
             files = [file.name for file in company_directory.glob('*') if file.is_file()]
@@ -194,7 +197,7 @@ UijGy2974VspHY+XrggzbKT2wzo8GNiFx16vuZdPNyXrZxUkqKjNZxpXvpJQ5YW6
         
         elif request.method == "DELETE":
             if not company_directory.is_dir():
-                return "Directory not found", 404
+                return jsonify({"message": f"Directory {str(company_directory)} not found"}), 404
             
             file_list = [file for file in company_directory.glob('*') if file.is_file()]
             for file in file_list:
@@ -202,229 +205,71 @@ UijGy2974VspHY+XrggzbKT2wzo8GNiFx16vuZdPNyXrZxUkqKjNZxpXvpJQ5YW6
                     
             if not any(company_directory.iterdir()):
                 company_directory.rmdir()
-                return "Files deleted successfully", 200
+                return jsonify({"message": f"All files from company {str(company_name)} deleted successfully"}), 200
             
-            return "Files deleted successfully", 200
-        
-        
-        
-                    # multiple files
-            # files = await request.files
-            # if 'file' not in files:
-            #     return 'No file', 401
-            # file_list = files.getlist('file')
-            # for file in file_list:
-            #     filename = file.filename
-            #     if filename == '':
-            #         continue
-            #         # return 'No file selected', 401
-            #     if file and allowed_file(filename):
-            #         file_directory = company_directory / secure_filename(filename)
-            #         if file_directory.is_file():
-            #             continue
-            #             # return 'file already exists', 409
-            #         await file.save(file_directory)
-            # config = get_config()
-            # add(config=config, source_directory=str(company_directory), collection_name=company_name) # does this make sense here? Or should it be done in a separate thread? Or should we use a seperate button for this?
-            # return "db created/updated successfully", 201
-        
-        
-        
+            return jsonify({"message": f"All files from company {str(company_name)} deleted successfully"}), 200
         
         
     # TODO: use str8labs media server
     @app.route('/files/<filename>', methods=["GET", "DELETE"])
     @jwt_required
     async def manage_file(filename):
-        jwt_identity = get_jwt_identity()
-        if not session_context[jwt_identity]["admin"]:
+        if not session["admin"]:
             return jsonify({"message": "Unauthorized, only allowed for admin"}), 401
-        company_name = session_context[jwt_identity]["company"]   
+        company_name = session["company"]   
         company_directory = DOCUMENT_DIRECTORY / company_name 
         
         if request.method == "GET":
             return await send_from_directory(company_directory,
                                filename)
             
-        # TODO: only allow for admin    
         elif request.method == "DELETE":
             if not company_directory.is_dir():
                 return "directory not found", 404
             file_directory = company_directory / secure_filename(filename)
             if file_directory.is_file():
                 file_directory.unlink()
-                return "File deleted successfully", 200
-            return "File not found", 404
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+                return jsonify({"message": f"File {str(filename)} deleted successfully"}), 200
+            return jsonify({"message": f"File {str(filename)} not found"}), 404
+        
 
     @app.route("/vectorstore", methods=["POST", "DELETE"])
     @jwt_required
-    async def manage_collection():
-        jwt_identity = get_jwt_identity()
+    async def vectorstore():
+        if not session["admin"]:
+            return jsonify({"message": "Unauthorized, only allowed for admin"}), 401
+        company_name = session["company"]  
+        company_directory = DOCUMENT_DIRECTORY / company_name
         
         if request.method == "POST":  
-            company_name = session_context[jwt_identity]["company"]          
-            company_directory = get_directory(company_name)
-            add(config=config, source_directory=str(company_directory), collection_name=company_name)
-            new_qa = get_retrieval_qa(config, llm, get_collection(config, company_name, embeddings))
-            session_context[jwt_identity]["qa"] = new_qa
-            return "added files to vectorstore successfully", 200
+            add(source_directory=str(company_directory), collection_name=company_name)
+            new_qa = get_retrieval_qa(config, llm, get_collection(company_name))
+            session_context["qa"] = new_qa
+            return jsonify({"message": f"added files to collection {str(company_name)} successfully"}), 200
     
         elif request.method == "DELETE":
-            delete(config=config, collection_name=company_name)
-            return "deleted collection successfully", 200
-        
-    # TODO
+            delete(collection_name=company_name)
+            new_qa = get_retrieval_qa(config, llm, get_collection(company_name))
+            session_context["qa"] = new_qa
+            return jsonify({"message": f"deleted collection {str(company_name)} successfully"}), 200
+            
+     
     @app.route("/vectorstore/<filename>", methods=["DELETE"])
     @jwt_required
-    async def delete_file_from_collection():
-        return "deleted file from collection successfully", 200
-
-        
-
+    async def delete_file_from_collection(filename):
+        if not session["admin"]:
+            return jsonify({"message": "Unauthorized, only allowed for admin"}), 401
+        company_name = session["company"] 
+        company_directory = DOCUMENT_DIRECTORY / company_name / filename
+        delete_file(company_name, str(company_directory))
+        new_qa = get_retrieval_qa(config, llm, get_collection(company_name))
+        session_context["qa"] = new_qa
+        return jsonify({"message": f"deleted file {str(filename)} from collection {str(company_name)} successfully"}), 200
             
 
     host, port = config["host"], config["port"]
     app.run(host=host, port=port, use_reloader=False, debug=True)
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    # TODO: use str8labs media server
-    # @app.route("/files", methods=["GET", "POST", "DELETE"])
-    # @jwt_required
-    # async def manage_files():
-        
-    #     company_name = get_jwt_identity()
-    #     if company_name not in company_dict:
-    #         return "company not initialized"
-    #     company_directory = company_dict[company_name]["source_directory"]
-        
-    #     # TODO: very slow. especially with large file size -> use chunking on client side + upload to cloud storage? (data security issues??)
-    #     # TODO: only allow for admin    
-    #     if request.method == "POST":
-            
-    #         # multiple files
-    #         # files = await request.files
-    #         # if 'file' not in files:
-    #         #     return 'No file', 401
-    #         # file_list = files.getlist('file')
-    #         # for file in file_list:
-    #         #     filename = file.filename
-    #         #     if filename == '':
-    #         #         continue
-    #         #         # return 'No file selected', 401
-    #         #     if file and allowed_file(filename):
-    #         #         file_directory = company_directory / secure_filename(filename)
-    #         #         if file_directory.is_file():
-    #         #             continue
-    #         #             # return 'file already exists', 409
-    #         #         await file.save(file_directory)
-    #         # config = get_config()
-    #         # add(config=config, source_directory=str(company_directory), collection_name=company_name) # does this make sense here? Or should it be done in a separate thread? Or should we use a seperate button for this?
-    #         # return "db created/updated successfully", 201
-            
-    #         req = await request.files
-    #         if 'file' not in req:
-    #             return 'No file', 401
-    #         file = req['file']
-    #         filename = file.filename
-    #         if filename == '':
-    #             return 'No file selected', 401
-    #         if file and allowed_file(filename):
-    #             file_directory = company_directory / secure_filename(filename)
-    #             if file_directory.is_file():
-    #                 return 'file already exists', 409
-    #             await file.save(file_directory)
-    #         return "file uploaded sucessfully", 201
-            
-                
-    #     elif request.method == "GET":
-    #         files = []
-    #         for file in company_directory.glob('*'):
-    #             if file.is_file():
-    #                 files.append(file.name)
-    #         return jsonify({"files": files}), 201
-        
-    #     # TODO: only allow for admin    
-    #     elif request.method == "DELETE":
-    #         if not company_directory.is_dir():
-    #             return "directory not found", 404
-    #         for file in company_directory.glob('*'):
-    #             if file.is_file():
-    #                 file.unlink()
-    #         company_directory.rmdir()
-    #         return "files deleted successfully", 200
-        
-    # # TODO: use str8labs media server
-    # @app.route('/files/<filename>', methods=["GET", "DELETE"])
-    # @jwt_required
-    # async def manage_file(filename):
-    #     jwt_identity = get_jwt_identity()
-    #     initUser(jwt_identity, request.headers)
-        
-    #     company_directory = company_dict[company_name]["source_directory"]
-        
-    #     if request.method == "GET":
-    #         return await send_from_directory(company_directory,
-    #                            filename)
-            
-    #     # TODO: only allow for admin    
-    #     elif request.method == "DELETE":
-    #         if not company_directory.is_dir():
-    #             return "directory not found", 404
-    #         file_directory = company_directory / secure_filename(filename)
-    #         if file_directory.is_file():
-    #             file_directory.unlink()
-    #             return "file deleted successfully", 200
-    #         return "file not found", 404
+
+
